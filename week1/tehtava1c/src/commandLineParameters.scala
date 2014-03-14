1f64733c2bb0f7d6ac197f07ed790d4389bae4eb
Miika Sirén <miika.siren@cs.helsinki.fi>
2014-03-14 14:05:17 +0200

    add function description

--- week1/tehtava1c/src/commandLineParameters.scala
+++ week1/tehtava1c/src/commandLineParameters.scala
@@ -2,4 +2,6 @@ object commandLineParameters extends App {
   	
 	args.sorted.foreach(println)
 	
+	//def sorted[B >: A](implicit ord: math.Ordering[B]): Array[T]
+	//Sorts this mutable indexed sequence according to an Ordering.
 }
